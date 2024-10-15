ed72234376c345d0a77db6b38378c8fa3b9b7a7c
Polvere243 <danielercoli@gmail.com>
2024-10-15 11:05:09 +0200

    used the event to target the parent element of the parent element

--- app.js
+++ app.js
@@ -4,3 +4,14 @@
 // seleziono i bottoni dal DOM
 
 const btns = document.querySelectorAll('.question-btn');
+
+// ciclo sui bottoni
+
+btns.forEach(btn => {
+    // aggancio un ascoltatore di eventi a tutti i bottoni
+    btn.addEventListener('click', e => {
+        // assegno una variabile all'elemento genitore dell'elemento genitore
+        const question = e.currentTarget.parentElement.parentElement;
+
+    });
+})
